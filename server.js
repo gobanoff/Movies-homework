f5e15eb397152c3056a07aa99c6fa0cc2af14ba0
gobanoff <a.kliosovas@yahoo.com>
2024-05-24 00:20:08 +0300

    fix

--- server.js
+++ server.js
@@ -9,17 +9,8 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-let movies = [{"id":1,"title":"Dune","production_date":"2024-05-08","producer":"Denis Villeneuve","rating":"8.5"},
-{"id":2,"title":"Fast X","production_date":"2024-04-29","producer":"Vin Diesel","rating":"7.1"},
-{"id":3,"title":"Oblivion","production_date":"2024-05-31","producer":"Joseph Kosinski","rating":"8.1"},
-{"id":4,"title":"Bloodshot","production_date":"2021-06-23","producer":"Neal Moritz","rating":"7.7"},
-{"id":5,"title":"Skyfall","production_date":"2024-06-01","producer":"Anthony Wade","rating":"7.8"},
-{"id":6,"title":"Kung Fu Panda 3","production_date":"2024-05-06","producer":"Alessandro Carloni","rating":"9.2"},
-{"id":7,"title":"No Time To Die","production_date":"2024-05-28","producer":"Barbara Broccoli","rating":"8.7"},
-{"id":8,"title":"Jason Bourne","production_date":"2024-06-05","producer":"Matt Damon","rating":"6.9"},
-{"id":9,"title":"Alien","production_date":"1978-06-23","producer":"Ridley Scott","rating":"9.3"},
-{"id":10,"title":"The Terminator","production_date":"1988-05-23","producer":"Gle Anne Hurd","rating":"9.1"}];
-let currentId = 11;
+let movies = [];
+let currentId = 1;
 
 function formatDate(date) {
     const d = new Date(date);
